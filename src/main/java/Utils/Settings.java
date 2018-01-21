6a622186a2a58f5ec73c9f1fffb210b5767564ca
Billy Rice <william.rice192@gmail.com>
2018-01-21 02:43:31 -0500

    Added a settings class
    added settings class to handle reading token from file and any further settings we may wish to include in the future

---
+++ src/main/java/Utils/Settings.java
@@ -0,0 +1,32 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Settings {
+    private static String token;
+    private static String path = "$PROJECT_DIR$/config.properties";
+
+    public static String getToken() {
+
+        Properties properties = new Properties();
+        InputStream inputStream;
+
+        try {
+            inputStream = new FileInputStream(path);
+            properties.load(inputStream);
+
+            token = properties.getProperty("token");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (token != null) {
+                return token;
+            }
+        }
+        return null;
+    }
+}
